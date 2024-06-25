f0501685acd9c76fdff0364b5dfc43dbbfe69e59
daniel <palm6991@gmail.com>
2024-06-25 23:11:55 +0200

    test-init

--- tests/ProductServiceTest.php
+++ tests/ProductServiceTest.php
@@ -9,33 +9,36 @@ use App\Domain\Entities\Product;
 
 class ProductServiceTest extends TestCase
 {
+    private $productRepository;
+    private $xmlReader;
+    private $logger;
+
+    protected function setUp(): void
+    {
+        $this->productRepository = $this->createMock(ProductRepositoryInterface::class);
+        $this->xmlReader = $this->createMock(XMLReader::class);
+        $this->logger = $this->createMock(LoggerInterface::class);
+    }
+
     public function testImportProducts()
     {
-        $productRepository = $this->createMock(ProductRepositoryInterface::class);
-        $xmlReader = $this->createMock(XMLReader::class);
-        $logger = $this->createMock(LoggerInterface::class);
-
         $product = new Product();
 
-        $xmlReader->method('read')->willReturn([$product]);
-        $productRepository->expects($this->once())->method('save')->with($product);
-        $logger->expects($this->once())->method('info')->with('Products imported successfully.');
+        $this->xmlReader->method('read')->willReturn([$product]);
+        $this->productRepository->expects($this->once())->method('save')->with($product);
+        $this->logger->expects($this->once())->method('info')->with('Products imported successfully.');
 
-        $productService = new ProductService($productRepository, $xmlReader, $logger);
+        $productService = new ProductService($this->productRepository, $this->xmlReader, $this->logger);
         $productService->importProducts(__DIR__ . '/fixtures/feed.xml');
     }
 
     public function testImportProductsWithNoProducts()
     {
-        $productRepository = $this->createMock(ProductRepositoryInterface::class);
-        $xmlReader = $this->createMock(XMLReader::class);
-        $logger = $this->createMock(LoggerInterface::class);
+        $this->xmlReader->method('read')->willReturn([]);
+        $this->productRepository->expects($this->never())->method('save');
+        $this->logger->expects($this->once())->method('warning')->with('No products found in the XML file.');
 
-        $xmlReader->method('read')->willReturn([]);
-        $productRepository->expects($this->never())->method('save');
-        $logger->expects($this->once())->method('warning')->with('No products found in the XML file.');
-
-        $productService = new ProductService($productRepository, $xmlReader, $logger);
+        $productService = new ProductService($this->productRepository, $this->xmlReader, $this->logger);
         $productService->importProducts(__DIR__ . '/fixtures/feed.xml');
     }
 }
